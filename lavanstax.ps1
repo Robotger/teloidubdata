034daaa3169a76b3c030d1aed0164f6234f03f3f
Ber4t Sensei <73439036+Ber4tBey@users.noreply.github.com>
2022-08-05 19:32:37 +0300

    Create lavanstax.ps1

---
+++ lavanstax.ps1
@@ -0,0 +1,27 @@
+Write-Output("@Lavanstax")
+Write-Output("⌛ Scoop Yükleniyor...")
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+Write-Output("⌛ İnstalling requirements...")
+scoop update
+scoop install nodejs --global
+scoop install git
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+Write-Output("⌛ İnstalling Lavanstax...")
+
+
+npm install 
+
+Write-Output(" @Lavanstax Windows Auto-Deployer ")
+
+Write-Output("⌛ İnstalling Python & Git ")
+scoop install git python@3.8
+
+Write-Output("⌛ İnstalling installer…")
+
+git clone https://github.com/LavanderProjects/Lavanderİnstaller
+Set-Location Lavanstaxİnstaller
+python3 -m pip install -r requirements.txt
+python3 -m lavan_installer
+
